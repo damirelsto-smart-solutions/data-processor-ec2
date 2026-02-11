1ec4514f68254fafb7d4d9b0360f2b21ece04965
Damir Trncic <D.trncic@elsto.eu>
2026-02-11 08:21:50 +0100

    Improvement-multiple-insert at once

--- elsto_data_processor.py
+++ elsto_data_processor.py
@@ -3,6 +3,7 @@ import pymysql
 from pydantic import BaseModel
 import json
 import datetime
+from typing import List, Union
 
 app = FastAPI()
 
@@ -15,16 +16,26 @@ db_config = {
     'port': 3306
 }
 
-class RobotEventRequest(BaseModel):
+class RobotEvent(BaseModel):
     event_date: datetime.date
     event_time: datetime.time
     kpi_name: str
     kpi_value: int
     kpi_explanation: str
     robot_id: int
+    
+class RobotEventRequest(BaseModel):
+    events: List[RobotEvent]
 
 @app.post("/log-robot-event")
-async def log_robot_event(request: RobotEventRequest):
+async def log_robot_event(request: Union[RobotEvent, RobotEventRequest]):
+    if isinstance(request, RobotEvent):
+        events = [request]
+    else:
+        events = request.events
+    if not events:
+        raise HTTPException(status_code=400, detail="No event provided")
+    
     connection = pymysql.connect(**db_config)
     try:
         with connection.cursor() as cursor:
@@ -33,16 +44,26 @@ async def log_robot_event(request: RobotEventRequest):
             (date, time, KPI_name, KPI_value, KPI_explanation, robot_id)
             VALUES (%s, %s, %s, %s, %s, %s)
             """
-            cursor.execute(sql, (
-                request.event_date,
-                request.event_time,
-                request.kpi_name,
-                request.kpi_value,
-                request.kpi_explanation,
-                request.robot_id
-            ))
+            
+            values = []
+            for event in events:
+                values.append((
+                    event.event_date,
+                    event.event_time,
+                    event.kpi_name,
+                    event.kpi_value,
+                    event.kpi_explanation,
+                    event.robot_id
+                ))
+                
+            cursor.executemany(sql, values)
         connection.commit()
-        return {"message": "Robot event logged successfully", "id": cursor.lastrowid}
+        return {
+            "message": "Robot event logged successfully", 
+            "id": cursor.lastrowid,
+            "event_count": len(events)
+            }
+        
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
     finally:
